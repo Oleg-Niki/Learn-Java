16ebac47a836056abdb0e28a5084e2d69df23ad8
Oleg Nikitashin <nikitashin.ov@gmail.com>
2024-09-03 10:33:56 -0500

    Create Homework2C.ino

---
+++ Homework2C.ino
@@ -0,0 +1,14 @@
+/** Description of program: This is my second program (Homework2C) prints a hello Oleg message on the screen
+ @author: Oleg Nikitashin
+ @since: 08/25/24
+ */
+public class Hello {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello World! Please write a name as a command-line argument.");
+        } else {
+            String name = args[0];
+            System.out.println("Hello World! My name is " + name);
+        }
+    }
+}
