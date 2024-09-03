a46627ceb08135d52ea374803a3e7a782c653da5
Oleg Nikitashin <nikitashin.ov@gmail.com>
2024-09-03 10:33:09 -0500

    Create Homework2A.ino

---
+++ Homework2A.ino
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is " + args[0]);
+        System.out.println("What is your name?");
+
+    }
+}
