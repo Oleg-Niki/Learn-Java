45eeda9f9d02814883df56322ed5a42449eae539
Oleg Nikitashin <nikitashin.ov@gmail.com>
2024-09-03 10:34:35 -0500

    Create HomeworkHello.ino

---
+++ HomeworkHello.ino
@@ -0,0 +1,9 @@
+/** Description of program: This is my fisrt program prints a hello message on the screen
+ @author: Oleg Nikitashin
+ @since: 08/22/24
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Oleg!");
+    }
+}
