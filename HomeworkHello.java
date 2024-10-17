2ea350178e6b59975231e0cf1df87f49237a81f4
Oleg Nikitashin <nikitashin.ov@gmail.com>
2024-10-17 09:26:42 -0700

    Delete HomeworkHello.java

--- HomeworkHello.java
+++
@@ -1,9 +0,0 @@
-/** Description of program: This is my fisrt program prints a hello message on the screen
- @author: Oleg Nikitashin
- @since: 08/22/24
- */
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Oleg!");
-    }
-}
