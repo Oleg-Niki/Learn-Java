2ff2050d8842511267b8fa53b6fa53c3880640bc
Oleg Nikitashin <nikitashin.ov@gmail.com>
2024-10-16 21:07:30 -0700

    Create Homework5.java

---
+++ Homework5.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        drawPic(args[0]);
+    }
+        static void drawPic(String c){
+            System.out.println("   " + c + c + c + c);
+            System.out.println("  "+ c + "    " + c);
+            System.out.println(" "+ c + "      " + c);
+            System.out.println("  " + c + "    " + c);
+            System.out.println("   " + c + c + c + c);
+        }
+}
