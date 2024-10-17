669cd1b6bc587970c9f5c0c81e9c3d4d0947a5ce
Oleg Nikitashin <nikitashin.ov@gmail.com>
2024-10-16 18:29:27 -0700

    Update Homework4.java

--- Homework4.java
+++ Homework4.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
+import java.util.Calendar;
 
 public class Homework4 {
     public static void main(String[] args) {
+        Calendar currentDate = Calendar.getInstance();
+        int currentYear = currentDate.get(Calendar.YEAR);
+        System.out.println("Hello User!");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello! My name is " + args[0]);
-        System.out.println("What is your name?");
+        System.out.println("What is you name?");
         String name = scanner.nextLine();
-        System.out.println("Nice to meet you" + name);
+        System.out.println("Nice to meet you, " + name);
+        System.out.println("What is your year of birth?");
+        int year = scanner.nextInt();
+        System.out.println("Cool, so you are :" + (currentYear - year) + " years old");
     }
 }
