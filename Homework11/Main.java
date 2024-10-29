4ba93934d3a18ec97d34607f4d99ef3e199e23b8
Oleg Nikitashin <nikitashin.ov@gmail.com>
2024-10-28 18:02:49 -0700

    Create Main.java
    (cherry picked from commit cd440ef8aac2c01c624406766c28425a2465ad62)

---
+++ Homework11/Main.java
@@ -0,0 +1,12 @@
+  public static void main(String[] args) {
+        ComplexNumber num1 = new ComplexNumber(2, 3);
+        ComplexNumber num2 = new ComplexNumber(1, -2);
+
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+
+        System.out.println("Addition: " + num1.add(num2));
+        System.out.println("Subtraction: " + num1.subtract(num2));
+        System.out.println("Multiplication: " + num1.multiply(num2));
+        System.out.println("Division: " + num1.divide(num2));
+    }
