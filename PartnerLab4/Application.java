f6c05e8f3c9667b1e637c2993f43c2d38da7d27a
Oleg Nikitashin <nikitashin.ov@gmail.com>
2024-10-29 19:04:25 -0500

    Update Application.java

--- PartnerLab4/Application.java
+++ PartnerLab4/Application.java
@@ -1,5 +1,5 @@
 package PartnerLab4;
-import java.util.Scanner;
+//import java.util.Scanner;
 /**
  * Bank Account Application Tester
  * @author Hellen Pacheco
@@ -7,15 +7,15 @@ import java.util.Scanner;
 public class Application {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to Bank!");
-        System.out.println("Please select an option:");
-        System.out.println("1. Deposit");
-        System.out.println("2. Withdrawal");
-        System.out.println("3. Statement");
-        int option = scanner.nextInt();
-        double amount = 0.0;
-        BankAccount account = new BankAccount(100);
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Welcome to Bank!");
+//        System.out.println("Please select an option:");
+//        System.out.println("1. Deposit");
+//        System.out.println("2. Withdrawal");
+//        System.out.println("3. Statement");
+//        int option = scanner.nextInt();
+//        double amount = 0.0;
+        PartnerLab4.BankAccount account = new PartnerLab4.BankAccount(100);
         System.out.println("New Account Balance: " + account.getBalance());
         System.out.println("Withdrawing 200");
         account.withdraw(200);
@@ -34,26 +34,27 @@ public class Application {
         System.out.println(account.getStatement());
 
 //here we can create a menu of options
-        switch (option) {
-            case 1:
-                System.out.println("For deposit enter amount in dollars: ");
-                double depositAmount = scanner.nextDouble();
-                if (depositAmount < 0) {
-                    System.out.println("Cannot be negative number.");
-                }
-                System.out.println("Your amount for depositing: " + depositAmount);
-
-
-                break;
-
-            case 2:
-                System.out.println("For withdraw enter amount in dollars: ");
-                break;
-
-            case 3:
-                System.out.println("Your account statement");
-                break;
-        }
-
+//        switch (option) {
+//            case 1:
+//                System.out.println("For deposit enter amount in dollars: ");
+//                double depositAmount = scanner.nextDouble();
+//                if (depositAmount < 0) {
+//                    System.out.println("Cannot be negative number.");
+//                }
+//                System.out.println("Your amount for depositing: " + depositAmount);
+//
+//
+//                break;
+//
+//            case 2:
+//                System.out.println("For withdraw enter amount in dollars: ");
+//                break;
+//
+//            case 3:
+//                System.out.println("Your account statement");
+//                break;
+//        }
+//
+//    }
     }
 }
