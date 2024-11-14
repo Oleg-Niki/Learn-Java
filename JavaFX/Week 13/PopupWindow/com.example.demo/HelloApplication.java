d7fd92c11f00959aa7b2d54d1d8ab73380b07512
Oleg Nikitashin <nikitashin.ov@gmail.com>
2024-11-14 12:06:13 -0800

    Update HelloApplication.java

--- JavaFX/Week 13/PopupWindow/com.example.demo/HelloApplication.java
+++ JavaFX/Week 13/PopupWindow/com.example.demo/HelloApplication.java
@@ -7,7 +7,15 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
 
 public class HelloApplication extends Application {
     @Override
@@ -31,5 +39,41 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+
+        Properties props = new Properties();
+        try {
+            // Load the app.properties file
+            FileInputStream fis = new FileInputStream("src/main/resources/com/example/demo/app.properties");
+            props.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Retrieve the properties
+        String host = props.getProperty("host");
+        String port = props.getProperty("port");
+        String database = props.getProperty("database");
+        String dbUser = props.getProperty("dbUser");
+        String dbPassword = props.getProperty("dbPassword");
+        String sslMode = props.getProperty("sslMode");
+        String sslRootCert = props.getProperty("sslRootCert");
+
+        // JDBC connection URL
+        String url = "jdbc:postgresql://" + host + ":" + port + "/" + database + "?ssl=" + sslMode;
+
+        if (sslMode.equals("require")) {
+            url += "&sslrootcert=" + sslRootCert;
+        }
+
+        try {
+            // Establish connection
+            Connection conn = DriverManager.getConnection(url, dbUser, dbPassword);
+            System.out.println("Connected to the database successfully!");
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
+
+
